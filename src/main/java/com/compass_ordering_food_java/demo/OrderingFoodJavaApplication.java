3091acb7ac762a88ead3a8817ee815299d63aafc
Daniil Chernousov <valerij.tehnik@yandex.ru>
2025-02-28 11:40:41 +0300

    first commit

---
+++ src/main/java/com/compass_ordering_food_java/demo/OrderingFoodJavaApplication.java
@@ -0,0 +1,13 @@
+package com.compass_ordering_food_java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingFoodJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingFoodJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compass_ordering_food_java/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass_ordering_food_java.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
